5cce2812750c9b0d3b6859722fb4d2085ac4002c
khazarglzd <khazarguluzade@gmail.com>
2024-10-03 16:23:59 +0400

    adding IncomeExpences & TransactionList component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Balance from './components/Balance'
 import { Header } from './components/Header'
 import "./App.css"
+import IncomeExpences from './components/IncomeExpences'
+import TransactionList from './components/TransactionList'
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Header />
       <div className='container'>
         <Balance />
+        <IncomeExpences />
+        <TransactionList />
       </div>
     </div>
   )
