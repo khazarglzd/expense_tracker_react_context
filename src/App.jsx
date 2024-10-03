763a354487fc2d3cab0f6f9e9613fe486dc04ee7
khazarglzd <khazarguluzade@gmail.com>
2024-10-03 23:46:14 +0400

    creating AddTransaction component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Header } from './components/Header'
 import "./App.css"
 import IncomeExpences from './components/IncomeExpences'
 import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Balance />
         <IncomeExpences />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   )
