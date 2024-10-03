f718a78bff31d3b4ab54ce7db95df036659b3f2a
khazarglzd <khazarguluzade@gmail.com>
2024-10-04 00:04:44 +0400

    first steps for creating Context

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,20 @@ import IncomeExpences from './components/IncomeExpences'
 import TransactionList from './components/TransactionList'
 import AddTransaction from './components/AddTransaction'
 
-const App = () => {
+import { GlobalProvider } from "./context/GlobalState"
+
+function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
-      <div className='container'>
+      <div className="container">
         <Balance />
         <IncomeExpences />
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
-  )
+    </GlobalProvider>
+  );
 }
 
-export default App
+export default App;
