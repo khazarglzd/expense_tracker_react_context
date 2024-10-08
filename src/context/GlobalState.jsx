cb84fc92b00ae550fbcc4d8d9e3610e65710d9e0
khazarglzd <khazarguluzade@gmail.com>
2024-10-09 00:02:03 +0400

    adding deleteTransaction action

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -22,12 +22,20 @@ const initialState = {
 // Create context
 export const GlobalContext = createContext(initialState);
 
+
 // Provider component
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
 
     return (<GlobalContext.Provider value={{
         transactions: state.transactions,
+        deleteTransaction
     }}>
         {children}
     </GlobalContext.Provider>);
